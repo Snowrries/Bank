b2575f5cae329571a0c084b63d08b998a3762f4e
Snowrries <Snowrries@users.noreply.github.com>
2015-04-09 00:57:37 -0400

    bit more effort
    desu

--- server.c
+++ server.c
@@ -22,9 +22,29 @@ static void set_iaddr(struct sockaddr_in * sockaddr, long x, unsigned int port){
 
 int socks(){
 	int sd;
+	int sporkd;
+	int addrlen;
 	pid_t pid;
 	struct sockaddr_in add;
-	
+	struct addrinfo	addrinfo;
+	struct addrinfo *result;
+	char message[256];
+	int on = 1;
+	const char *port;
+	port = "What's our port?";
+
+	addrinfo.ai_flags = AI_PASSIVE;		// for bind()
+	addrinfo.ai_family = AF_INET;		// IPv4 only
+	addrinfo.ai_socktype = SOCK_STREAM;	// Want TCP/IP
+	addrinfo.ai_protocol = 0;		// Any protocol
+	addrinfo.ai_addrlen = 0;
+	addrinfo.ai_addr = NULL;
+	addrinfo.ai_canonname = NULL;
+	addrinfo.ai_next = NULL;
+	if ( getaddrinfo( 0, port, &addrinfo, &result ) != 0 ){
+		fprintf( stderr, "\x1b[1;31mgetaddrinfo( %s ) failed errno is %s.  File %s line %d.\x1b[0m\n", port, strerror( errno ), __FILE__, __LINE__ );
+		return -1;
+	}
 	if((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
 		printf("Socketing failed. errno: %d", errno);
 		return -1;
@@ -37,6 +57,13 @@ int socks(){
 		printf("Listen failed. Speak up or I have to put in my hearing aids. Errno: %d", errno);
 
 	}
+	
+	while(1){
+		addrlen = sizeof(struct sockaddr_storage);
+		sporkd = accept(sd, )
+	}
+	
+	
 	pid = fork();
 	if(pid == 0){
 		signal(/*SIGSOMETHING*/,ChildSigHandler);
