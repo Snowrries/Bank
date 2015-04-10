aee8c46634a399284a76ff3631a71ee987372c2e
Snowrries <Snowrries@users.noreply.github.com>
2015-04-09 20:52:15 -0400

    Interim
    So I don't lose progress (again)

--- server.c
+++ server.c
@@ -5,9 +5,9 @@
 
 void ChildSigHandler(int signal){
 
-	while( (wait(0)) != -1){
-
-	}
+	//while( (wait(0)) != -1){
+	//}
+	
 }
 
 
@@ -45,11 +45,12 @@ void *client_session_thread(void* arg){
 int socks(){
 	int sd;
 	int sporkd;
-	int addrlen;
-	pid_t pid;
+	int pid;
 	struct sockaddr_in add;
 	struct addrinfo	addrinfo;
 	struct addrinfo *result;
+	socklen_t addrlen;
+	struct sockaddr_storage them;
 	char message[256];
 	int on = 1;
 
@@ -65,30 +66,64 @@ int socks(){
 		fprintf( stderr, "\x1b[1;31mgetaddrinfo( %s ) failed errno is %s.  File %s line %d.\x1b[0m\n", CLIENT_PORT, strerror( errno ), __FILE__, __LINE__ );
 		return -1;
 	}
-	if((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
+	/*if((sd = socket(AF_INET, SOCK_STREAM, 0)) == -1){
 		printf("Socketing failed. errno: %d", errno);
 		return -1;
 	}
 	if((bind(sd, (struct sockaddr *)&add, sizeof(struct sockaddr))) == -1){
 		printf("Bind failed. errno: %d", errno);
 		return -2;
+	}*/
+	else if ( setsockopt( sd, SOL_SOCKET, SO_REUSEADDR, &on, sizeof(on) ) == -1 )
+	{
+		printf( "setsockopt() failed in %s()\n", func );
+		return 0;
 	}
-	if(listen(sd,128) == -1){
-		printf("Listen failed. Speak up or I have to put in my hearing aids. Errno: %d", errno);
-
+	else if ( set_iaddr( &addr, INADDR_ANY, CLIENT_PORT ), errno = 0,
+			bind( sd, (const struct sockaddr *)&addr, sizeof(addr) ) == -1 )
+	{
+		printf( "bind() failed in %s() line %d errno %d\n", func, __LINE__, errno );
+		close( sd );
+		return 0;
+	}
+	else if ( listen( sd, 100 ) == -1 )
+	{
+		printf( "Listen failed. Speak up or I have to put in my hearing aids.\nlisten() failed in %s() line %d errno %d\n", func, __LINE__, errno );
+		close( sd );
+		return 0;
+	}
+	
+	else
+	{
+		ic = sizeof(senderAddr);
+		while ( (sporkd = accept( sd, (struct sockaddr *)&senderAddr, &ic )) != -1 )
+		{
+			fdptr = (int *)malloc( sizeof(int) );
+			*fdptr = sporkd;					// pointers are not the same size as ints any more.
+		}
+		close( sd );
+		return 0;
 	}
 	
 	while(1){
 		addrlen = sizeof(struct sockaddr_storage);
-		sporkd = accept(sd, )
+		sporkd = accept(sd,(struct sockaddr *)&them, &addrlen);
+		pid = fork();
+		if(pid == -1)
+		{
+			perror("Forking error.");
+			close(sporkd); // Make sure this is ok
+		}
+		else if(pid == 0)//Is Child process
+		{
+//			signal(SIGCHLD,ChildSigHandler); ?? 
+		}
+		else//Is parent process
+		{
+			printf("Created child process %d\n", pid);
+			close(sporkd);
+		}
 	}
-	
-	
-	pid = fork();
-	if(pid == 0){
-		signal(SIGCHLD,ChildSigHandler);
-	}
-
-
-	return sd;
+	return 0;
+	//return sd;
 }
