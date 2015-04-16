9e0d46a08c2bcbd38a8d52593837022c474331a9
Snowrries <Snowrries@users.noreply.github.com>
2015-04-16 04:45:39 -0400

    bit of code exported
    look at session.c

--- server.c
+++ server.c
@@ -2,7 +2,7 @@
 #include "server.h"
 
 
-int shmid;
+account_t* p;
 int readers;
 sem_t read;
 sem_t write;
@@ -27,87 +27,6 @@ void ChildSigHandler(int signal){
 
 void periodic_printing(int signal, siginfo_t *ignore, void *ignore2){
 	//Print account info every 20 seconds. Raise a signal in Server main function.
-}
-typedef enum {
-	create,
-	serve,
-	deposit,
-	withdraw,
-	query,
-	end,
-	quit
-} inpt;
-void client_session(int sd){
-	char *buffer;
-	char *command;
-	char *arguments;
-	char request[2048];
-	char* storage;
-	char temp;
-	int i;
-	int curr,size;
-	float ignore;
-	long senderIPaddr;
-
-	while(1){
-		curr = 0;
-		size = 0;
-		while((size = recv(sd,request,sizeof(request),0)) > 0){
-			curr += size;
-			if(curr > 2048){
-				storage = "Overflow input. Please enter another command.";
-				write( sd, storage, strlen(storage) + 1 );
-				continue;
-			}
-			strncpy(storage, request, size);
-			
-		}
-		//Here, we have a line of input from client. Let's decipher it.
-		sscanf(storage, "%sm %sm", &command, &arguments);
-		/*Check validity for command, switch, then check argument validity. */
-
-		buffer = malloc(sizeof(char)*9);
-		memcpy(buffer, command, sizeof(char)*9);
-		buffer[8] = '\0';
-		//Maybe consider converting to lowercase instead of telling them to type in lower.
-		if(strcmp(buffer, "create") == 0){
-			sem_wait(read);
-			sem_wait(welcome);
-			sem_wait(write);
-			
-			//Uh...
-			sem_post(welcome);
-			sem_post(read);	
-			sem_post(write);
-		}
-		else if(strcmp(buffer, "serve") == 0){
-			
-		}
-		else if(strcmp(buffer, "deposit") == 0){
-			
-		}
-		else if(strcmp(buffer, "withdraw") == 0){
-			
-		}
-		else if(strcmp(buffer, "query") == 0){
-			readers++;
-			if(readers == 1){//If first reader, lock write.
-				sem_wait(write);
-			}
-		}
-		else if(strcmp(buffer, "end") == 0){
-			
-		}
-		else if(strcmp(buffer, "quit") == 0){
-			
-		}
-		else{
-			printf("Please enter a valid command, in all lowercase. ");
-		}
-		
-		
-	}
-
 }
 
 
@@ -199,13 +118,7 @@ int socks(const char* port){
 				close(sporkd);
 			}
 		}
-
-
-
-
 	}
-
-
 	return 0;
 	//return sd;
 }
@@ -213,10 +126,10 @@ int socks(const char* port){
 void sharingcaring(){
 	/* Shared Memory Section*/
 
-//	int shmid; //Is declared globally
-
+	int shmid;
+	//	account_t* p; Is declared globally
 	key_t key;
-	account_t* p;
+
 	int size;
 	size = 20 * sizeof(account_t); // 20 accounts
 	if(errno = 0, (key = ftok("testplan.txt",42)) == -1){
